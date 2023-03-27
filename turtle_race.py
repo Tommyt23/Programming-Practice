6e3292c625f6ffc5b085412b0a11f8cd087d15e2
Tommyt23 <tom@ewok.co.uk>
2023-03-27 09:51:44 +0100

    upload turtle _race

---
+++ turtle_race.py
@@ -0,0 +1,38 @@
+# Using tutorial from futurelearn.com
+from turtle import Turtle
+from random import randint
+
+a = Turtle()
+b = Turtle()
+c = Turtle()
+d = Turtle()
+
+a.color('red')
+a.shape('turtle')
+a.penup()
+a.goto(-160, 100)
+a.pendown()
+
+b.color('blue')
+b.shape('turtle')
+b.penup()
+b.goto(-160, 70)
+b.pendown()
+
+c.color('green')
+c.shape('turtle')
+c.penup()
+c.goto(-160, 40)
+c.pendown()
+
+d.color('yellow')
+d.shape('turtle')
+d.penup()
+d.goto(-160, 10)
+d.pendown()
+
+for movement in range(100):
+    a.forward(randint(1,5))
+    b.forward(randint(1,5))
+    c.forward(randint(1,5))
+    d.forward(randint(1,5))
